0060ea5f766976a72f297910523f92abe2d8ef1f
dovydaszulkus <dovydaszulkus@gmail.com>
2022-07-09 22:34:40 +0100

    Add Prisma

---
+++ express-api/prisma/migrations/20220709212808_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "poster" TEXT NOT NULL,
+    "status" TEXT NOT NULL
+);
--- express-api/server.ts
+++ express-api/server.ts
@@ -1,14 +1,20 @@
-import express from 'express'
 import "./config/dotenv"
+import express from 'express'
+import { PrismaClient } from "@prisma/client"
 
 const app = express()
+const prisma = new PrismaClient()
 
 const PORT = process.env.PORT
 
 app.use(express.json())
 
-app.get('/api', (req, res) => {
+app.get('/api', async (req, res) => {
   console.log('/api was called')
+
+  const movies = await prisma.movie.findMany()
+
+  console.log('movies', movies)
 
   res.status(200).json({
     hello: "world",
